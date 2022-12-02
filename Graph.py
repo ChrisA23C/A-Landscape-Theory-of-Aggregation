cb334dd90e0dd9bf71e12e9c47b4214f0f33aa96
Cherry <ezhik2233@gmail.com>
2022-12-02 19:05:05 +0300

    Fourth optimizaton

--- Graph.py
+++ Graph.py
@@ -3,19 +3,19 @@ import numpy as np
 import time
 from itertools import *
 
-def printmatrix(matrix):
+def printmatrix(matrix): #Красивый вывод матрицы
     for i in matrix:
         s = ''
         for j in i:
             s+=str(j)+' '*(4-len(str(j)))
         print(s)
 
-def genericmatrix(n,maxw):
+def genericmatrix(n,maxw): #Генерация матрицы
     matrix = np.random.randint(-maxw, maxw+1, (n,n))
     np.fill_diagonal(matrix, 0)
     return matrix
 
-def genericmatrixones(n):
+def genericmatrixones(n): #Генерация матрицы плюс-минус единиц
     matrix = np.array([])
     for i in range(n):
         row = np.array([])
@@ -27,19 +27,15 @@ def genericmatrixones(n):
     np.fill_diagonal(matrix, 0)
     return matrix
 
-def genericallcombones(n):
-    arrs = np.array([])
-    combs = list(product([1,-1], repeat=(n*(n-1))))
-    for i in combs:
+def genericallcombones(n): #Генерация всех возможных матриц из единиц и минус единиц
+    for i in product([1,-1], repeat=(n*(n-1))):
         arr = np.array(list(i))
         for j in range(0,n**2+1,n+1):
             arr = np.insert(arr,j,0)
         arr = np.reshape(arr,(n,n))
-        arrs = np.append(arrs,arr)
-    arrs = np.reshape(arrs,(int(len(arrs)/(n*n)),n,n))
-    return arrs
+        yield arr
 
-def genericcomb(n):
+def genericcomb(n): #Генерация всех возможных коалиций
     s = ''.join(list(map(str,range(0,n))))
     setopt = []
     for i in range(2,len(s)+1):
@@ -47,7 +43,7 @@ def genericcomb(n):
             setopt.append(list(map(int, j)))
     return setopt
  
-def gencombmatrix(n,setopt):
+def gencombmatrix(n,setopt): #Генерация матрицы всех возможных коалиций
     v = np.ones(n,int)
     combmatrix = np.array([])
     for i in setopt:
@@ -58,32 +54,27 @@ def gencombmatrix(n,setopt):
     combmatrix.shape = int(len(combmatrix)/n),n
     return combmatrix
     
-def psearch(matrix,combmatrix,setopt):
+def psearch(matrix,combmatrix,setopt,p): #Поиск устойчивых коалиций
     x = np.matmul(combmatrix,matrix)
     x = x*combmatrix
     arr = np.amin(x, axis = 1)
     arr2 = np.where(arr >= 0)[0]
-    #print('Sustainable coalitions: ',np.array(setopt,dtype='object')[arr2])
+    if p == True: print('Sustainable coalitions: ',np.array(setopt,dtype='object')[arr2])
 
-def main(matrix,setopt,combmatrix):
-    #print(matrix)
-    psearch(matrix,combmatrix,setopt)
+def main(matrix,setopt,combmatrix,p):
+    psearch(matrix,combmatrix,setopt,p)
     
 #const     
 n = 4
 maxw = 1
+countprint = 1000
 ###
-
-#matrix = genericmatrix(n,maxw)
-#matrix = genericmatrixones(n)
-arrs = genericallcombones(n)
 setopt = genericcomb(n)
 combmatrix = gencombmatrix(n,setopt)
-#matrix = np.array([[0,1,2],[1,0,-1],[2,-1,0]])
-#start_time = time.time()
-#main(n,matrix)
 start_time = time.time()
-for i in arrs:
-    main(i,setopt,combmatrix)
+print(2**(n*(n-1)),'iterations expected')
+for count,i in enumerate(genericallcombones(n)):
+    if int(count%countprint)==0: print(count,'-',(time.time() - start_time),'seconds')
+    main(i,setopt,combmatrix,False)
 print('Search time = %s seconds' % (time.time() - start_time))
 
